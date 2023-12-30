201845009cbcad6816feb4850c1010058fc086fc
MKPBimsara <125986754+MKPBimsara@users.noreply.github.com>
2023-12-30 16:08:36 +0000

    Final commit

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -66,12 +66,22 @@ public class DataSource {
     return account;
   }
 
-  public static void main(String[] args) {
-    Customer customer = getCustomer("twest8o@friendfeed.com");
-    System.out.println(customer.getName());
+  public static void updateAccountBalance(int accountId, double balance){
+    String sql = "update account set balance = ? where id = ?";
 
-    Account account = getAccount(customer.getAccountId());
-    System.out.println(account.getBalance());
+    try(
+      Connection connection = connect();
+      PreparedStatement statement = connection.prepareStatement(sql)
+    ){
+
+      statement.setDouble(1, balance);
+      statement.setInt(2, accountId);
+
+      statement.executeUpdate();
+      
+    }catch(SQLException e){
+      e.printStackTrace();
+    }
   }
 
 }
